a93551dfc6a83dd707693542c1ffa8c5e4e5dd76
TheWisePigeon <zozozozeph@gmail.com>
2023-02-04 18:16:41 +0000

    add certus file reader

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -1,10 +1,12 @@
+
 pub fn interpreter( certus_file: &str ){
     let mut certus_file_content = "";
     let file_reader = std::fs::read_to_string(certus_file);
     match file_reader {
         Ok(content)=>{
-            println!("{}", content);
             certus_file_content = &content;
+            let raw_lines: Vec<&str> = certus_file_content.split("\n").collect();
+            println!("{:?}", raw_lines);
         },
         Err(_)=>{
             println!("Something went wrong 🥲\nAre you sure this file exists?");
